9ea7ce4a22bd35c2e93121e0be24ea6c382dd0fc
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-13 17:58:49 +0300

    product details ui styled

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,73 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Stack, useLocalSearchParams } from 'expo-router'
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
-  const {id} = useLocalSearchParams()
-  return (
-    <View>
-      <Stack.Screen options={{title: "Details:"+ id}} />
-      <Text style={{fontSize: 20}}>Product Id is: {id}</Text>
-    </View>
-  )
-}
+  const { id } = useLocalSearchParams();
 
-export default ProductDetailsScreen
+  const product = products.find((product) => product.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product Not found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <View style={styles.size} key={size}>
+            <Text style={styles.sizeText}>{size}</Text>
+          </View>
+        ))}
+      </View>
+      <Text style={styles.price}>{product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    padding: 10,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default ProductDetailsScreen;
