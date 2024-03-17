6609a85ffa20fc8da4dcc1b647f32d01e288cb74
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-17 22:39:45 +0300

    create component added style to center

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
 const CreateProductScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>create</Text>
     </View>
-  )
-}
+  );
+};
 
-export default CreateProductScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+});
+
+export default CreateProductScreen;
