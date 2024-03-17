d17713df9077be1a697cec300e2112b3d0edbe9c
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-17 22:48:39 +0300

    create component name, price, create button

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,10 +1,22 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import React from "react";
+import Button from "@/components/Button";
 
 const CreateProductScreen = () => {
+  const onCreate = (): void => {};
   return (
     <View style={styles.container}>
-      <Text>create</Text>
+      <Text style={styles.label}>Name</Text>
+      <TextInput placeholder="Name" style={styles.input} />
+
+      <Text style={styles.label}>Price ($)</Text>
+      <TextInput
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType="numeric"
+      />
+
+      <Button onPress={onCreate} text="Create" />
     </View>
   );
 };
@@ -14,6 +26,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     padding: 10,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
+  },
+  label: {
+    color: "gray",
+    fontSize: 16,
   },
 });
 
