47858bcdb6e17f000ae908b3ff1beb83faa3af75
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-18 15:18:42 +0300

    ordersPage Product Details UI completed

---
+++ src/app/(user)/orders/[id].tsx
@@ -0,0 +1,21 @@
+import OrderListItem from "@/components/OrderListItem";
+import orders from "@assets/data/orders";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function OrderDetailsScreen() {
+  const { id } = useLocalSearchParams();
+
+  const order = orders.find((o) => o.id.toString() === id);
+
+  if (!order) {
+    return <Text>Not found</Text>;
+  }
+
+  return (
+    <View style={{padding: 10}}>
+      <Stack.Screen options={{ title: `Order ${id}` }} />
+      <OrderListItem order={order} />
+    </View>
+  );
+}
