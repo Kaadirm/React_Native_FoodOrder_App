86336892f652c246f219cffe0937e267bc430e1e
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-18 09:13:24 +0300

    create product adding image functions and component title changed to "Create Product"

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,18 +1,23 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Image } from "react-native";
 import React, { useState } from "react";
 import Button from "@/components/Button";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import Colors from "@/constants/Colors";
+import * as ImagePicker from "expo-image-picker";
+import { Stack } from "expo-router";
 
 const CreateProductScreen = () => {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [errors, setErrors] = useState("");
+  const [name, setName] = useState<string>("");
+  const [price, setPrice] = useState<string>("");
+  const [errors, setErrors] = useState<string>("");
+  const [image, setImage] = useState<string | null>(null);
 
-  const resetFields = () => {
+  const resetFields = (): void => {
     setName("");
     setPrice("");
   };
 
-  const validateInput = () => {
+  const validateInput = (): boolean => {
     setErrors("");
     if (!name) {
       setErrors("Name is required");
@@ -38,8 +43,33 @@ const CreateProductScreen = () => {
     // Save in the database
     resetFields();
   };
+
+  // Image Selection start
+  const pickImage = async (): Promise<void> => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+  // Image Selection end
+
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ title: "Create Product" }} />
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select Image
+      </Text>
       <Text style={styles.label}>Name</Text>
       <TextInput
         value={name}
@@ -67,6 +97,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     padding: 10,
+  },
+  image: {
+    width: "50%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
   },
   input: {
     backgroundColor: "white",
